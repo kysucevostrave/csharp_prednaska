102e6ded0c32329830f2e1813ab7e656e0c199ca
Edering <matejdurana0@gmail.com>
2021-10-12 16:22:43 +0200

    Add project files.

---
+++ csharp_prednaska/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace csharp_prednaska
+{
+
+    public struct Pair
+    {
+        public int x;
+        public int y;
+        public Pair(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Pair p = new Pair(1,2);
+            p.x = 12;
+            p.y = 3;
+        }
+    }
+}
